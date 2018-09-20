a48b39afd4985d40404ed0a31761d67e0c936862
Damian Kanak <pgs-dkanak@pc32.home>
2018-09-20 16:25:38 +0200

    Display star buttons

---
+++ StarView/StarView.swift
@@ -0,0 +1,64 @@
+//
+//  StarView.swift
+//  StarView
+//
+//  Created by Damian Kanak on 20/09/2018.
+//  Copyright © 2018 Damian Kanak. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class StarView: UIView {
+    
+    @IBInspectable var numberOfStars : Int = 1
+    @IBInspectable var normalImage : UIImage = UIImage()
+    @IBInspectable var highlightedImage : UIImage = UIImage()
+    
+    private var buttons = [UIButton]()
+    
+    /*
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+     // Drawing code
+     }
+     */
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func layoutStars() {
+        self.subviews.forEach { $0.removeFromSuperview() }
+        
+        for buttonNumber in 0..<numberOfStars {
+            let buttonWidth = self.frame.width / CGFloat(numberOfStars)
+            let buttonHeight = self.frame.height
+            
+            let buttonFrame = CGRect(x: buttonWidth * CGFloat(buttonNumber), y: 0, width: buttonWidth, height: buttonHeight).integral
+            let button = UIButton(frame: buttonFrame)
+            
+            button.setImage(normalImage, for: .normal)
+            button.setImage(highlightedImage, for: .selected)
+            button.setImage(highlightedImage, for: .highlighted)
+            
+            buttons.append(button)
+            self.addSubview(button)
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layoutStars()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        layoutStars()
+    }
+}
+
